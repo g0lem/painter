e72e2bbebe17c02b5478d26183cbad46ab85931a
g0lem <sasteve999@gmail.com>
2018-05-01 19:58:26 +0300

    indent

--- script.js
+++ script.js
@@ -7,48 +7,47 @@
 //*                                                     *
 //*******************************************************
 
-window.onload = function() {
+window.onload = function () {
 
     var canvas = document.getElementById('mainCanvas');
     var context = canvas.getContext('2d');
 
 
-                                            //*********************************************
-    const defaultColor = [255,255,255,255]; //*** white, since you delete it with white ***
-                                            //*********************************************
+    //*********************************************
+    const defaultColor = [255, 255, 255, 255]; //*** white, since you delete it with white ***
+                                               //*********************************************
 
-                                            //***************************
+    //***************************
     var paintColor;                         //*** current brush color ***
                                             //***************************
 
 
     /*******************************************
-    *  declaring the elements for further use  *
-    ********************************************/
+     *  declaring the elements for further use  *
+     ********************************************/
     var widthInputElement = document.getElementById('width');
     var heightInputElement = document.getElementById('height');
 
 
     /******************************************
-    * setting the default values of the cells *
-    *******************************************/
+     * setting the default values of the cells *
+     *******************************************/
     var cellWidth = widthInputElement.value;
     var cellHeight = heightInputElement.value;
 
 
     /******************************************
-    *     update the cells if they change     *
-    *******************************************/
-    widthInputElement.onchange = function(){
+     *     update the cells if they change     *
+     *******************************************/
+    widthInputElement.onchange = function () {
         cellWidth = this.value;
     };
 
-    heightInputElement.onchange = function(){
-        cellHeight= this.value;
+    heightInputElement.onchange = function () {
+        cellHeight = this.value;
     };
 
 
-   
 //*******************************************************
 //*******************  Colors   *************************
 //*******************************************************
@@ -62,24 +61,23 @@ window.onload = function() {
 //*******************************************************
 
 
-
     /************************************************
-    * converts a hex color string into a rgba array *
-    ************************************************/
-    var hexToRGB = function(hex, alpha=255) {
+     * converts a hex color string into a rgba array *
+     ************************************************/
+    var hexToRGB = function (hex, alpha = 255) {
         var r = parseInt(hex.slice(1, 3), 16);
         var g = parseInt(hex.slice(3, 5), 16);
         var b = parseInt(hex.slice(5, 7), 16);
 
-        return [r,g,b,alpha];
+        return [r, g, b, alpha];
     };
 
     /*****************************************
-    * sees if two color arrays are the same  *
-    ******************************************/
-    var compareColors = function(color1, color2){
-        for(var i=0; i<4; i++){
-            if(color1[i] !== color2[i]){
+     * sees if two color arrays are the same  *
+     ******************************************/
+    var compareColors = function (color1, color2) {
+        for (var i = 0; i < 4; i++) {
+            if (color1[i] !== color2[i]) {
                 return false;
             }
         }
@@ -106,21 +104,21 @@ window.onload = function() {
 
 
     /********************************
-    * colors a pixel on the canvas  *
-    *********************************/
-    ImageData.prototype.colorPixel = function(color, index = 0){
-        for(var i=0;i<color.length;i++){
-            this.data[index+i] = color[i];  
+     * colors a pixel on the canvas  *
+     *********************************/
+    ImageData.prototype.colorPixel = function (color, index = 0) {
+        for (var i = 0; i < color.length; i++) {
+            this.data[index + i] = color[i];
         }
     };
 
     /******************************************
-    *  get the color of a pixel of the canvas *
-    *******************************************/
-    ImageData.prototype.getColor = function(index = 0){
+     *  get the color of a pixel of the canvas *
+     *******************************************/
+    ImageData.prototype.getColor = function (index = 0) {
         var color = [];
-        for(var i=0;i<4;i++){
-            color.push(this.data[index+i]);  
+        for (var i = 0; i < 4; i++) {
+            color.push(this.data[index + i]);
         }
         return color;
     };
@@ -135,9 +133,9 @@ window.onload = function() {
 //*******************************************************
 
     /**********************************************
-    *  gets mouse (x,y) coordonates in the canvas *
-    ***********************************************/
-    var getMousePosition = function(event) {
+     *  gets mouse (x,y) coordonates in the canvas *
+     ***********************************************/
+    var getMousePosition = function (event) {
         var rect = canvas.getBoundingClientRect();
         return {
             x: event.clientX - rect.left,
@@ -146,11 +144,11 @@ window.onload = function() {
     };
 
     /***************************************************************
-    *  this rounds up the mouse coords to reflect the cell clicked *
-    ****************************************************************/
-    var roundUpMousePosition = function(position){
-        position.x = Math.floor(position.x/cellWidth)*cellWidth;
-        position.y = Math.floor(position.y/cellHeight)*cellHeight;
+     *  this rounds up the mouse coords to reflect the cell clicked *
+     ****************************************************************/
+    var roundUpMousePosition = function (position) {
+        position.x = Math.floor(position.x / cellWidth) * cellWidth;
+        position.y = Math.floor(position.y / cellHeight) * cellHeight;
         return position;
     };
 
@@ -165,11 +163,11 @@ window.onload = function() {
 //*******************************************************
 
     /******************************
-    *  this colors an entire cell *
-    *******************************/
-    var colorCell = function(position, imageData, color){
-        for(var i=0; i<cellHeight; i++){
-            for(var j=0; j<cellWidth; j++){
+     *  this colors an entire cell *
+     *******************************/
+    var colorCell = function (position, imageData, color) {
+        for (var i = 0; i < cellHeight; i++) {
+            for (var j = 0; j < cellWidth; j++) {
                 imageData.colorPixel(color, getIndex(position.x + j, position.y + i));
             }
         }
@@ -177,14 +175,14 @@ window.onload = function() {
     }
 
     /************************************************************************
-    *  converts (x,y) coordonates into an index, since the data is an array *
-    *************************************************************************/
-    var getIndex = function(x,y){
+     *  converts (x,y) coordonates into an index, since the data is an array *
+     *************************************************************************/
+    var getIndex = function (x, y) {
         return 4 * (x + y * canvas.offsetHeight);
     };
 
 
-    var evaluateProperColor = function(mousePos, imageData){
+    var evaluateProperColor = function (mousePos, imageData) {
         var normalColor = hexToRGB(document.getElementById('color').value);
         var cellColor = imageData.getColor(getIndex(mousePos.x, mousePos.y));
         paintColor = compareColors(normalColor, cellColor) ? defaultColor : normalColor;
@@ -192,23 +190,23 @@ window.onload = function() {
 
 
     /*********************************************************************
-    *  this handles the color picking process and fills the current cell *
-    *********************************************************************/
-    var onMouseDownEvent = function(event){
-        var imageData = context.getImageData(0,0,canvas.offsetWidth,canvas.offsetHeight);
+     *  this handles the color picking process and fills the current cell *
+     *********************************************************************/
+    var onMouseDownEvent = function (event) {
+        var imageData = context.getImageData(0, 0, canvas.offsetWidth, canvas.offsetHeight);
         var mousePos = roundUpMousePosition(getMousePosition(event));
-        evaluateProperColor(mousePos,imageData);
-        context.putImageData(colorCell(mousePos, imageData, paintColor), 0, 0);       
+        evaluateProperColor(mousePos, imageData);
+        context.putImageData(colorCell(mousePos, imageData, paintColor), 0, 0);
     };
 
 
     /**************************************************************************
-    *  this handles mouse movement and the coloring of the cells you run over *
-    ***************************************************************************/
-    var onMouseMoveEvent = function(event){
-        var imageData = context.getImageData(0,0,canvas.offsetWidth,canvas.offsetHeight);
+     *  this handles mouse movement and the coloring of the cells you run over *
+     ***************************************************************************/
+    var onMouseMoveEvent = function (event) {
+        var imageData = context.getImageData(0, 0, canvas.offsetWidth, canvas.offsetHeight);
         var mousePos = roundUpMousePosition(getMousePosition(event));
-        if(event.buttons !== 0){
+        if (event.buttons !== 0) {
             context.putImageData(colorCell(mousePos, imageData, paintColor), 0, 0);
         }
 
@@ -216,8 +214,8 @@ window.onload = function() {
 
 
     /***************************************************************
-    *  binding the listeners on 'mousemove' and 'mousedown' events *
-    ****************************************************************/
+     *  binding the listeners on 'mousemove' and 'mousedown' events *
+     ****************************************************************/
     canvas.addEventListener('mousemove', onMouseMoveEvent);
     canvas.addEventListener('mousedown', onMouseDownEvent);
 
